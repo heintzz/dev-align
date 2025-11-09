4065d20cfdb4ccb75b5c226f387a207d106d34cc
IDJAK132_ Rahadi Fauzan <rahadifauzan7@gmail.com>
2025-11-09 12:25:43 +0700

    fix: unselect team when create project

--- Frontend/src/components/Loading.jsx
+++ Frontend/src/components/Loading.jsx
@@ -1,6 +1,6 @@
 // components/Loading.jsx
 
-import { FolderKanban } from "lucide-react";
+import logoIcon from "@/assets/img/LogoDevAlign.png";
 
 export default function Loading({
   status,
@@ -20,7 +20,7 @@ export default function Loading({
           <div className="animate-spin rounded-full h-16 w-16 border-b-4 border-primer"></div>
 
           <div className="absolute inset-0 flex items-center justify-center">
-            <FolderKanban className="w-6 h-6 text-primer" />
+            <img src={logoIcon} alt="DevAlign Logo" className="h-8 w-8" />
           </div>
         </div>
         <p className="mt-4 text-white font-medium">{text}</p>
